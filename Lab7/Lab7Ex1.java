56ada1fb203cba2fb5426e7f63a8d77b73ce90cf
Anton Samoilov <absamoilov88@gmail.com>
2019-10-01 11:40:34 +0100

    Lab1 update

--- Lab7/Lab7Ex1.java
+++ Lab7/Lab7Ex1.java
@@ -1,6 +1,11 @@
 public class Lab7Ex1 {
-    private static void outputResult(double num, int max) {
-        for (int i = 0; i < max; i++) {
+    private static void outputResult(double num, int min, int max) {
+        if (min >= max || min < 0 || max < 0) {
+            System.out.println("Incorrect input");
+            return;
+        }
+
+        for (int i = min; i <= max; i++) {
             double m = num * i;
             String result = String.format("%5f x %2d = %8f", num, i, m);
             System.out.println(result);
@@ -8,6 +13,6 @@ public class Lab7Ex1 {
     }
 
     public static void main(String[] args) {
-        outputResult(8.5678, 99);
+        outputResult(8.5678, 0, 99);
     }
 }
