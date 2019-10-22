c75a82e21ea3eef282ccdeaa2626bc66330f7969
Anton Samoilov <absamoilov88@gmail.com>
2019-10-22 12:33:52 +0100

    Clean up

--- Lab8_Objects/Lab8Ex2.java
+++
@@ -1,5 +0,0 @@
-public class Lab8Ex2 {
-    public static void main(String[] args) {
-        
-    }
-}
--- Lab8_Objects/Lab8Ex3.java
+++
@@ -1,5 +0,0 @@
-public class Lab8Ex3 {
-    public static void main(String[] args) {
-        
-    }
-}
